9e111024ddeda431a86ca23cf55062087169bb2c
Dimoon336 <152537131+Dimoon336@users.noreply.github.com>
2024-02-15 02:22:05 +0200

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,7 @@
+function response(answer) {
+    var result = document.getElementById('result');
+    
+    if (answer === 'yes') {
+      result.innerHTML = "That's wonderful! Happy Valentine's Day!";
+    } 
+  }
