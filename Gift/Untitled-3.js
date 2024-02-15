732f210a3f5d0f0d7dcd6dd6640326b118afd6a1
Dimoon336 <152537131+Dimoon336@users.noreply.github.com>
2024-02-15 02:29:10 +0200

    Delete Gift directory

--- Gift/Untitled-3.js
+++
@@ -1,7 +0,0 @@
-function response(answer) {
-    var result = document.getElementById('result');
-    
-    if (answer === 'yes') {
-      result.innerHTML = "That's wonderful! Happy Valentine's Day!";
-    } 
-  }
